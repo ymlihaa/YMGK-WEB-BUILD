53466f93dad538f1b1a5c889e1a95a7e193874e1
ymlihaa <ymlihaa@gmail.com>
2019-11-04 18:27:44 +0300

    Login componenti icerisindeki handlesubmit fonksiyonu ile validate fonksiyonu birleştirildi.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,27 @@
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Media from "react-bootstrap/Media";
-import logo from "../logo.png";
+import { PageHeader } from "antd";
+
 function Header() {
   return (
-    <Navbar className="navbar" variant="light" bg="light">
-      <Navbar.Brand>
-        <Media>
-          <img width={70} height={70} src={logo} alt="Generic placeholder" />
-          <Media.Body>
-            <h1>FIRAT ÜNİVERSİTESİ</h1>
-          </Media.Body>
-        </Media>
-      </Navbar.Brand>
-    </Navbar>
+    // <Navbar className="navbar" variant="light" bg="light">
+    //   <Navbar.Brand>
+    //     <Media>
+    //       <img width={70} height={70} src={logo} alt="Generic placeholder" />
+    //       <Media.Body>
+    //         <h1>FIRAT ÜNİVERSİTESİ</h1>
+    //       </Media.Body>
+    //     </Media>
+    //   </Navbar.Brand>
+    // </Navbar>
+
+    <div>
+      <PageHeader
+        style={{
+          border: "1px solid rgb(235, 237, 240)"
+        }}
+        title="FIRAT ÜNİVERSİTESİ"
+      />
+    </div>
   );
 }
 export default Header;
